8073e06a191627f570bd9c2546cc8fe44ae4a238
unknown <vedikasangle@gmail.com>
2023-10-05 22:47:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes')
+
+require('dotenv').config();
+require('./db')
+const PORT = 8000;
+
+app.use(bodyParser.json());
+app.use('/users', userRoutes );
+app.use('/tasks', taskRoutes);
+
+//Path : index.js
+app.get('/', (req,res)=>{
+    res.json({
+        message : 'Task Manager API is working'
+
+    })
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
